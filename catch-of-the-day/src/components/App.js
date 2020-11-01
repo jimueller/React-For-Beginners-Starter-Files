b51173081e0f7b669a26eb266a42ac169bdd7729
Jim Mueller <jim@jimueller.com>
2020-10-31 22:59:31 -0500

    Done with 15 - Displaying State

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
+import Fish from "./Fish";
 
 class App extends Component {
   state = {
@@ -10,7 +12,6 @@ class App extends Component {
   };
 
   addFish = (fish) => {
-    console.log("Adding a fish", fish);
     // 1. take a copy of the existing state (never mutate state)
     const fishes = { ...this.state.fishes };
     // 2. add new fish
@@ -19,14 +20,26 @@ class App extends Component {
     this.setState({ fishes });
   };
 
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
         <div className="menu">
           <Header tagline="Jim is cool" />
+          <ul className="fishes">
+            {Object.keys(this.state.fishes).map((key) => (
+              <Fish key={key} fish={this.state.fishes[key]} />
+            ))}
+          </ul>
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
