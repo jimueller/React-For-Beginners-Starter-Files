403d2b41668a6bdbb603c6680b0f54c0833511ec
Jim Mueller <jim@jimueller.com>
2020-10-31 20:02:51 -0500

    Done with 13 - Understanding State

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -1,9 +1,24 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
 
-class App extends Component{
+class App extends Component {
+  state = {
+    fishes: {},
+    order: {},
+  };
+
+  addFish = (fish) => {
+    console.log("Adding a fish", fish);
+    // 1. take a copy of the existing state (never mutate state)
+    const fishes = { ...this.state.fishes };
+    // 2. add new fish
+    fishes[`fish${Date.now()}`] = fish;
+    // 3. set the new fishes object in state
+    this.setState({ fishes });
+  };
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -11,9 +26,9 @@ class App extends Component{
           <Header tagline="Jim is cool" />
         </div>
         <Order />
-        <Inventory />
+        <Inventory addFish={this.addFish} />
       </div>
-    )
+    );
   }
 }
 
--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -2,11 +2,31 @@ import React from 'react';
 import {getFunName} from "../helpers";
 
 class StorePicker extends React.Component {
+  /* old school
+  constructor(){
+      super();
+      this.goToStore = this.goToStore.bind(this);
+  }
+   */
+
+  // Refs (simple handle on components)
+  storeNameInput = React.createRef();
+
+  // Setting as property so this is bound without using the constructor bind
+  goToStore = event => {
+    // 1. stop default submit
+    event.preventDefault();
+    // 2. get the store name from the ref
+    const storeName = this.storeNameInput.current.value;
+    // 3. change url
+    this.props.history.push(`/store/${storeName}`);
+  };
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={this.goToStore}>
         <h2>Please Enter A Store</h2>
-        <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+        <input type="text" ref={this.storeNameInput} required placeholder="Store Name" defaultValue={getFunName()}/>
         <button type="submit">Visit Store </button>
       </form>
     );
